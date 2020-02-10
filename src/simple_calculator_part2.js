dd5812e7c076d7988a846370062f829f4a75585f
Sbonelo01 <sbonelomkhize15@gmail.com>
2020-02-10 10:29:57 +0200

    minor spec modifications

--- src/simple_calculator_part2.js
+++ src/simple_calculator_part2.js
@@ -1,20 +1,19 @@
-
 let Calculator = class {
     constructor() {
         this.arr = [];
         this.last = 0;
     }
-    
-    add() {
+
+    add(...args) {
         var sum = 0;
         //Loop through each number while adding it
-        for(var counter = 0; counter < arguments.length; counter++) { 
-            if(arguments[counter] == 'LAST') {
-                arguments[counter] = this.last;
+        for (var counter = 0; counter < args.length; counter++) {
+            if (args[counter] == 'LAST') {
+                args[counter] = this.last;
             }
 
-            sum += arguments[counter];
-            arguments[counter] = this.last;
+            sum += args[counter];
+            args[counter] = this.last;
             this.last = sum;
         }
 
@@ -22,28 +21,28 @@ let Calculator = class {
         return sum;
     }
 
-    multiply() {
+    multiply(...args) {
         var product = 1;
 
-        for (var counter = 0; counter < arguments.length; counter++) {
-            if(arguments[counter] == 'LAST'){
-                arguments[counter] = this.last;
+        for (var counter = 0; counter < args.length; counter++) {
+            if (args[counter] == 'LAST') {
+                args[counter] = this.last;
             }
 
-            product *= arguments[counter];
-            arguments[counter] = this.last;
+            product *= args[counter];
+            args[counter] = this.last;
             this.last = product;
         }
-        
+
         return product;
     }
 
-    stringSlot(string){
-        if(string == 'LAST'){
+    stringSlot(string) {
+        if (string == 'LAST') {
             return this.last();
         }
-        if(string.includes('SLOT_') && !isNaN(string[string.length -1])){
-            var slotNumer = string[string.length -1];
+        if (string.includes('SLOT_') && !isNaN(string[string.length - 1])) {
+            var slotNumer = string[string.length - 1];
             return this.arrSlots[slotNumber];
         }
         return 0;
@@ -51,18 +50,19 @@ let Calculator = class {
 
     //last adds/multiplies a given argument by the last returned value
     last() {
-      return this.lastValue;
-     }
+        return this.lastValue;
+    }
 
-     setSlot(reg) {
-         this.arrSlots[slotNUmber] = this.lastValue;
-     }
+    setSlot(reg) {
+        this.arrSlots[slotNumber] = this.lastValue;
+    }
 
-     getSlot(slotNumber) {
+    getSlot(slotNumber) {
         return this.arrSlots[slotNumber];
-     }
+    }
 }
 
-module.exports = { 
-    Calculator 
+
+module.exports = {
+    Calculator
 }
